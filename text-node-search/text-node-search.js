1101121ef39958c22b84fe345d4e3ec8912843e0
seanlum <seanjklum@gmail.com>
2020-06-03 09:25:11 -0700

    Update text-node-search
        1. Added a function to generate search fields with reset buttons
        2. Modified the code to allow multiple search fields to coexist
        3. Added a config usage for result isolation.
        4. Did some testing to validate that three searches can add and
           remove results without affecting results from other searches.
    
        Usage:
    
    	To create a new search form:
    
    		window.createTextNodeSearch({
    			'resultGroup' : <string: name of results>,
    			'searchPattern' : <regular expression>,
    			'tempElementType': <string: element name>,
    			'resultElementType': <string: element name>,
    			'resultClass': <string: element class>,
    			'resultIdPrefix': <string: result prefix>
    		});
    
    	Example:
    
    		window.createTextNodeSearch({
    			'resultGroup': 'Example Group',
    			'searchPattern': 'vitae|porta|eget',
    			'tempElementType': 'result-x',
    			'resultElementType': 'strong',
    			'resultClass': 'x-temp-search-result',
    			'resultIdPrefix': 'x-tsr-'
    		})
    
    	This produces the following at the top of the page:
    		- A text input field with the supplied expression
    		- A "Search" button to mark the results of that
    		  search.
    		- A "Remove Results" button to remove the results
    		  of that search.

--- text-node-search/text-node-search.js
+++ text-node-search/text-node-search.js
@@ -8,88 +8,120 @@
 */
 (function() {
     document.addEventListener('DOMContentLoaded', function(domContentLoadedEvent) {
-        let searchPattern = new RegExp(document.getElementById('search-field').value, 'g');
 
-        function addNodesOf(node) {
-            let nodesToCollect = [];
-            if (node.hasChildNodes()) {
-                for (let nodeIndex = 0; nodeIndex < node.childNodes.length; nodeIndex++) {
-                    nodesToCollect.push({
-                        new : true,
-                        node : node.childNodes[nodeIndex],
-                        parent : node,
-                        replaced : false
-                    });
-                }
-                return nodesToCollect;
-            } else {
-                return false;
-            }
+        if (window.textNodeSearch === undefined) {
+            window.textNodeSearch = {};
         }
 
-        function processNode(data) {
-            if (data.node.nodeValue && data.node.nodeValue.match(searchPattern)) {
-                if (data.node.innerHTML) {
-                    data.node.innerHTML = data.node.nodeValue.replace(searchPattern, function(match) {
-                        return '<strong class="temp-search-result">' + match + '</strong>';
-                    });
+        function createTextNodeSearch(config) {
+            window.textNodeSearch[config.resultGroup].config = config;
+            this.defaultPattern = 'change';
+            this.searchPattern = new RegExp(config.searchPattern ? config.searchPattern : this.defaultPattern, 'g');
+
+            function addNodesOf(node) {
+                let nodesToCollect = [];
+                if (node.hasChildNodes()) {
+                    for (let nodeIndex = 0; nodeIndex < node.childNodes.length; nodeIndex++) {
+                        nodesToCollect.push({
+                            new : true,
+                            node : node.childNodes[nodeIndex],
+                            parent : node
+                        });
+                    }
+                    return nodesToCollect;
                 } else {
-                    let newNode = document.createElement('result');
-                    newNode.innerHTML = data.node.nodeValue.replace(searchPattern, function(match) {
-                        return '<strong class="temp-search-result">' + match + '</strong>';
-                    });
-                    data.parent.replaceChild(newNode, data.node);
+                    return false;
                 }
-
             }
-        }
 
-        function processDocumentBody() {
-            let bodyNodes = addNodesOf(document.body);
-            for (let bodyIndex = 0; bodyIndex < bodyNodes.length; bodyIndex++) {
-                if (bodyNodes[bodyIndex].new === true) {
-                    bodyNodes[bodyIndex].new = false;
-                    let childNodes = addNodesOf(bodyNodes[bodyIndex].node);
-                    if (childNodes === false) {
-                        processNode(bodyNodes[bodyIndex]);
-                    } else if (childNodes.length) {
-                        for (let childIndex=0; childIndex<childNodes.length; childIndex++) {
-                            bodyNodes.push(childNodes[childIndex]);
-                        } 
+            function processNode(data) {
+                if (data.node.nodeValue && data.node.nodeValue.match(this.searchPattern)) {
+                    if (data.node.innerHTML) {
+                        data.node.innerHTML = data.node.nodeValue.replace(this.searchPattern, function(match) {
+                            return '<' + config.resultElementType +
+                                   ' class="' + config.resultClass + '">' +
+                                   match + '</' + config.resultElementType + '>';
+                        });
+                    } else {
+                        let newNode = document.createElement(config.tempElementType);
+                        newNode.innerHTML = data.node.nodeValue.replace(this.searchPattern, function(match) {
+                            return '<' + config.resultElementType +
+                                   ' class="' + config.resultClass + '">' +
+                                   match + '</' + config.resultElementType + '>';
+                        });
+                        data.parent.replaceChild(newNode, data.node);
                     }
                 }
             }
-            let results = document.getElementsByClassName('temp-search-result');
-            for (let resultIndex = 0; resultIndex < results.length; resultIndex++) {
-                results[resultIndex].id = 'tsr-n' + resultIndex
-            }
-        }
 
-        function removeResult() {
-            let results = undefined;
-            while ((results = document.getElementsByClassName('temp-search-result')).length > 0) {   
+            function processDocumentBody() {
+                let bodyNodes = addNodesOf(document.body);
+                for (let bodyIndex = 0; bodyIndex < bodyNodes.length; bodyIndex++) {
+                     if (bodyNodes[bodyIndex].new === true) {
+                         bodyNodes[bodyIndex].new = false;
+                        let childNodes = addNodesOf(bodyNodes[bodyIndex].node);
+                        if (childNodes === false) {
+                            processNode(bodyNodes[bodyIndex]);
+                        } else if (childNodes.length) {
+                            for (let childIndex=0; childIndex<childNodes.length; childIndex++) {
+                                bodyNodes.push(childNodes[childIndex]);
+                            }
+                        }
+                    }
+                }
+                let results = document.getElementsByClassName(config.resultClass);
                 for (let resultIndex = 0; resultIndex < results.length; resultIndex++) {
-                    let replacementNode = document.createTextNode(results[resultIndex].innerText);
-                    results[resultIndex].parentNode.replaceChild(replacementNode, results[resultIndex]);
+                    results[resultIndex].id = config.resultIdPrefix + resultIndex;
                 }
             }
-            results = undefined;
-            while ((results = document.getElementsByTagName('result')).length > 0) {
-                for (let resultIndex = 0; resultIndex < results.length; resultIndex++) {
-                    let replacementNode = document.createTextNode(results[resultIndex].innerText);
-                    results[resultIndex].parentNode.replaceChild(replacementNode, results[resultIndex]);
+
+            function removeResults() {
+                let results = undefined;
+                while ((results = document.getElementsByClassName(config.resultClass)).length > 0) {
+                    for (let resultIndex = 0; resultIndex < results.length; resultIndex++) {
+                        let replacementNode = document.createTextNode(results[resultIndex].innerText);
+                        results[resultIndex].parentNode.replaceChild(replacementNode, results[resultIndex]);
+                    }
+                }
+                results = undefined;
+                while ((results = document.getElementsByTagName(config.tempElementType)).length > 0) {
+                    for (let resultIndex = 0; resultIndex < results.length; resultIndex++) {
+                        let replacementNode = document.createTextNode(results[resultIndex].innerHTML);
+                        results[resultIndex].parentNode.replaceChild(replacementNode, results[resultIndex]);
+                    }
                 }
             }
+
+            function startTextNodeSearch() {
+                this.searchPattern = new RegExp(document.getElementById(config.resultIdPrefix + '-search-field').value, 'g');
+                removeResults();
+                processDocumentBody();
+            }
+
+            let startTextNodeSearchBind = startTextNodeSearch.bind(this);
+            let removeResultsBind = removeResults.bind(this);
+
+            window.textNodeSearch[config.resultGroup].removeResults = removeResults;
+
+            let textNodeSearchForm = document.createElement('div');
+
+            let textNodeSearchField = document.createElement('input');
+                textNodeSearchField.id = config.resultIdPrefix + '-search-field';
+                textNodeSearchField.value = config.searchPattern ? config.searchPattern : this.defaultPattern;
+            let textNodeSearchStartButton = document.createElement('button');
+                textNodeSearchStartButton.innerText = "Search";
+                textNodeSearchStartButton.onclick = startTextNodeSearchBind;
+            let textNodeSearchResetButton = document.createElement('button');
+                textNodeSearchResetButton.innerText = "Remove Results";
+                textNodeSearchResetButton.onclick = removeResultsBind;
+
+            textNodeSearchForm.appendChild(textNodeSearchField);
+            textNodeSearchForm.appendChild(textNodeSearchStartButton);
+            textNodeSearchForm.appendChild(textNodeSearchResetButton);
+
+            document.body.insertBefore(textNodeSearchForm, document.body.children[0]);
         }
 
-        window.removeResult = removeResult;
-
-        processDocumentBody();
-
-        window.processRegexSearch = function(searchFieldEvent) {
-            searchPattern = new RegExp(document.getElementById('search-field').value, 'g');
-            removeResult();
-            processDocumentBody();
-        }
+        window.createTextNodeSearch = createTextNodeSearch;
     });
 })()
